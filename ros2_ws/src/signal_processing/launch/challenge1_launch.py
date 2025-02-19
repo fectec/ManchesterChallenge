438add720a6a5df222a153e5188e9ab9da46225c
fectec <fectec151@gmail.com>
2025-02-19 15:21:30 +0000

    Sinusoidal waves generated

--- ros2_ws/src/signal_processing/launch/challenge1_launch.py
+++ ros2_ws/src/signal_processing/launch/challenge1_launch.py
@@ -3,15 +3,21 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    signal_generator_node = Node(package='signal_processing'),
+    signal_generator_node = Node(package='signal_processing',
                                 executable='signal_generator',
                                 output='screen'
                                 )
 
-    signal_processor_node = Node(package='signal_processing'),
+    signal_processor_node = Node(package='signal_processing',
                                 executable='signal_processor',
                                 output='screen'
                                 )
-
-    l_d = LaunchDescription([signal_generator_node, signal_processor_node])
+                                
+    rqt_node = Node(name='rqt_plot',
+	           package='rqt_plot',
+	           executable='rqt_plot',
+	           arguments=['/signal/data', '/proc_signal/data']
+	           )
+    
+    l_d = LaunchDescription([signal_generator_node, signal_processor_node, rqt_node])
     return l_d
